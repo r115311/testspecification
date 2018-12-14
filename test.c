75fbed7b97d82441e061a4d35ea35b4ba3d5734e
r115311 <43838592+r115311@users.noreply.github.com>
2018-12-14 09:22:18 +0900

    added trash,hoge function

--- test.c
+++ test.c
@@ -1,5 +1,13 @@
 #include<stdio.h>
 #include<string.h>
+void trash(){
+	printf("trash関数\n");
+}
+
+void hoge(){
+	printf("hoge関数\n");
+}
+
 int main(){
 	char a[6][8];
 	char b[6][8];
@@ -11,6 +19,8 @@ int main(){
 	strcat(a[0],b[0]);
 	printf("%s\n",a[0]);
 	printf("denshi\n");
+	trash();
+	hoge();
 	printf("EXIT\n");
 	return 0;
 }
